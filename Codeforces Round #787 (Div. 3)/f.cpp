faf40c99a7c2bb3d8d66ce7d1906def40cc6d634
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2022-05-15 16:19:17 -0400

    feat: add solution to problem f of CF-787

--- Codeforces Round #787 (Div. 3)/f.cpp
+++ Codeforces Round #787 (Div. 3)/f.cpp
@@ -4,32 +4,21 @@ using namespace std;
 
 typedef long long ll;
 
-const int maxn = 41;
-const int oo = 1e9;
+vector<int> pi;
+vector<vector<int>> g;
+vector<bool> seen;
 
-int dp[maxn][maxn][maxn][maxn];
-int n, A, B;
-string x;
-
-int solve(int pos, int remA, int remB, int dif)
+void dfs(int u, int p)
 {
-    if (pos == n)
+    for (auto &v : g[u])
     {
-        if (dif == n || dif == 0)
-            return oo;
-        if (remA != 0 || remB != 0)
-            return oo;
-        return abs(dif - (n - dif));
+        if (v != p)
+        {
+            pi[v] = u;
+            dfs(v, u);
+        }
     }
-
-    if (dp[pos][remA][remB][dif] != -1)
-        return dp[pos][remA][remB][dif];
-
-    int s1 = solve(pos + 1, (remA * 10 + x[pos] - '0') % A, remB, dif + 1);
-    int s2 = solve(pos + 1, remA, (remB * 10 + x[pos] - '0') % B, dif);
-
-    return dp[pos][remA][remB][dif] = min(s1, s2);
-}
+};
 
 int main()
 {
@@ -42,37 +31,54 @@ int main()
 
     while (t--)
     {
-        cin >> n >> A >> B;
-        cin >> x;
-        memset(dp, -1, sizeof(dp));
-        int ans = solve(0, 0, 0, 0);
 
-        if (ans == oo)
-            cout << -1 << "\n";
-        else
+        int n, k, x, y;
+        cin >> n >> k >> x >> y;
+
+        g = vector<vector<int>>(n + 1);
+        seen = vector<bool>(n + 1);
+        pi = vector<int>(n + 1);
+
+        set<int> s;
+        for (int i = 0; i < k; i++)
         {
-            int pos = 0, remA = 0, remB = 0, dif = 0;
-            string rec = "";
-            while (pos < n)
-            {
-                int s1 = solve(pos + 1, (remA * 10 + x[pos] - '0') % A, remB, dif + 1);
-                int s2 = solve(pos + 1, remA, (remB * 10 + x[pos] - '0') % B, dif);
-                if (s1 < s2)
-                {
-                    rec += "R";
-                    dif++;
-                    remA = (remA * 10 + x[pos] - '0') % A;
-                }
-                else
-                {
-                    rec += "B";
-                    remB = (remB * 10 + x[pos] - '0') % B;
-                }
-                pos++;
-            }
-
-            cout << rec << "\n";
+            int u;
+            cin >> u;
+            s.insert(u);
         }
+
+        for (int i = 1, a, b; i < n; i++)
+        {
+            cin >> a >> b;
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+
+        dfs(x, 0);
+
+        int ans = 0;
+
+        int cur = y;
+        while (y != x)
+        {
+            ans++;
+            seen[y] = true;
+            y = pi[y];
+        }
+        seen[x] = true;
+
+        for (auto &u : s)
+        {
+            int v = u;
+            while (!seen[v])
+            {
+                seen[v] = true;
+                ans += 2;
+                v = pi[v];
+            }
+        }
+
+        cout << ans << endl;
     }
 
     return 0;
